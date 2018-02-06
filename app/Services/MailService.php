e9d5fec5d23a268be2accb06c5fe7a0ea5d46fbd
badry-abderrahmane <32793907+badry-abderrahmane@users.noreply.github.com>
2018-02-06 12:34:34 +0000

    Updated MailService

---
+++ app/Services/MailService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use Mail;
+
+class MailService
+{
+
+
+  public function send($email){
+    try {
+      Mail::send('emails.support', ['title' => $email->title, 'content' => $email->body], function ($message) use($email)
+      {
+          $message->replyTo($email->address, $name = null);
+
+          $message->subject($email->subject);
+
+          $message->priority($email->level);
+          $message->from($email->from, 'STG Maroc');
+          $message->to($email->to);
+      });
+    } catch (\Exception $e) {
+      return 'Problem Sending Email';
+    }
+
+  return 'Mail Sent';
+  }
+
+
+
+}
